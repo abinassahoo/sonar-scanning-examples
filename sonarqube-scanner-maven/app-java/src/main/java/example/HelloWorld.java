5eb66f3a84d369e780019ab56a0a5944f3eeb1c5
abinassahoo <abinas.online@live.com>
2019-05-29 15:19:47 +0530

    Added bug

--- sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
+++ sonarqube-scanner-maven/app-java/src/main/java/example/HelloWorld.java
@@ -1,6 +1,9 @@
 package example;
 
 public class HelloWorld {
+  
+  int age;
+  String uname;
 
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
@@ -15,6 +18,10 @@ public class HelloWorld {
 
   public void notCovered() {
     System.out.println("notCovered");
+      for (i=0; i<numrows; i++)
+  for (j=0; j<numcols; j++);
+    pixels++;
   }
+
 
 }
